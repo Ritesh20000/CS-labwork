59cd618e2a7b12e898ee6fef3f99247b05128fee
Ritesh20000 <42208407+Ritesh20000@users.noreply.github.com>
2018-08-16 16:30:54 +0530

    Add files via upload

---
+++ lab2_q2.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "what is your name?"
+read RITESH
+mkdir $RITESH
+touch $RITESH/$RITESH"2".cpp $RITESH/$RITESH"3".cpp $RITESH/$RITESH"4".cpp $RITESH/$RITESH"5".cpp $RITESH/$RITESH"1".cpp
+
+
+
