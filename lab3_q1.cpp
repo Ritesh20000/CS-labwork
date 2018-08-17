515409191b3203f9ad96c07f713c7dbe96595365
Ritesh20000 <42208407+Ritesh20000@users.noreply.github.com>
2018-08-17 10:42:01 +0530

    Add files via upload

---
+++ lab3_q1.cpp
@@ -0,0 +1,36 @@
+//lab 2 q1
+//include library
+#include <iostream>
+using namespace std;
+//declaring values of all types,1st question
+//integer
+int main ()
+{
+
+int a=4;
+cout << a <<endl;
+
+//float
+float b=3.1410;
+cout << b <<endl;
+//bool
+bool c=a==b;
+cout <<c <<endl;
+//char
+char d=1;
+cout <<d <<endl;
+//double
+double e=5.123456789e298;
+cout <<e <<endl;
+//printing out the sizes of data types
+
+cout << sizeof(int) << endl;
+cout << sizeof(float) << endl;
+cout << sizeof(bool) << endl;
+cout <<sizeof(char) << endl;
+cout <<sizeof(double) << endl;
+
+return 0;
+//concludes q1
+}
+
