4da11113298c286b25507228569f11083e3ac79b
Ritesh20000 <42208407+Ritesh20000@users.noreply.github.com>
2018-08-17 11:15:32 +0530

    Add files via upload

---
+++ lab3_q2.cpp
@@ -0,0 +1,60 @@
+//lab2_q2
+//include library
+#include <iostream>
+using namespace std;
+//Question 2, declaring 2 values and use arithmetic operators on them
+int main()
+{
+//declare variables
+int a,b;
+a=2;
+b=3;
+//sum
+int SUM;
+
+SUM= a+b;
+cout <<SUM <<endl;
+//product
+int product;
+
+product= a*b;
+cout <<product <<endl;
+//difference
+int difference;
+difference=a-b;
+cout <<difference <<endl;
+//division
+int quotient;
+quotient= a/b;
+cout <<quotient <<endl;
+//modulus
+int modulus;
+modulus= a%b;
+cout <<modulus <<endl;
+
+//FLOAT values
+float c,d;
+c=2.134;
+d=3.143;
+//sum
+float SUMfloaT;
+SUMfloaT=c+d;
+cout <<SUMfloaT <<endl;
+//product
+float ProductfloaT;
+ProductfloaT=c*d;
+cout <<ProductfloaT <<endl;
+//divison
+float QuotientfloaT;
+QuotientfloaT=c/b;
+cout <<QuotientfloaT <<endl;
+//difference
+float differencefloaT;
+differencefloaT=c-d;
+cout <<differencefloaT <<endl;
+
+
+
+//concludes q2
+return 0;
+}
